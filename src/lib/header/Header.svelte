9f5343ef3e7b47d892659e4bb060d31126fb63a2
Matthew Mirman <matt@extensional.ai>
2022-10-05 17:13:03 +0200

    tried again

--- src/lib/header/Header.svelte
+++ src/lib/header/Header.svelte
@@ -16,14 +16,8 @@
 		</svg>
 		<ul>
 			<li class:active={$page.url.pathname === '/'}><a sveltekit:prefetch href="/">Home</a></li>
-			<li class:active={$page.url.pathname.startsWith('/codact/')}>
-				<a sveltekit:prefetch href="/codact/init">Codact</a>
-			</li>
 			<li class:active={$page.url.pathname === '/about'}>
 				<a sveltekit:prefetch href="/about">About</a>
-			</li>
-			<li class:active={$page.url.pathname === '/todos'}>
-				<a sveltekit:prefetch href="/todos">Todos</a>
 			</li>
 		</ul>
 		<svg viewBox="0 0 2 3" aria-hidden="true">
