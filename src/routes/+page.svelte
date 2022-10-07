4667a0ebccd515d6e47bb15f9f79acf70a523468
Matthew Mirman <matt@extensional.ai>
2022-10-07 21:05:29 +0200

    fixed the newchat

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -116,10 +116,11 @@
       <input type="hidden" name="selectionStart" value={selectionStart.toString()} />
       <input type="hidden" name="selectionEnd" value={selectionEnd.toString()} />
       <input type="hidden" name="selection" value="" />
-      <input
+      > <input
+        class="question"
         name="question"
         aria-label="ask codact a question"
-        placeholder="codact> How can I help you?"
+        placeholder="How can I help you?"        
       />
     </form>
   </div>
@@ -149,44 +150,31 @@
     margin: 0 0 0.5rem 0;
   }
 
-  input {
-    border: 1px solid transparent;
+  .newchat input {
+    padding: 1em 0em 0em 0em;
+    width: 90%;
+    border: none;
+    background: rgba(255, 255, 255, 0.05);
   }
 
   input:focus-visible {
-    box-shadow: inset 1px 1px 6px rgba(0, 0, 0, 0.1);
-    border: 1px solid #ff3e00 !important;
+    border: none;
     outline: none;
   }
 
-  .newchat input {
-    font-size: 28px;
-    width: 100%;
-    padding: 0.5em 1em 0.3em 1em;
-    box-sizing: border-box;
-    background: rgba(255, 255, 255, 0.05);
-    border-radius: 8px;
-    text-align: center;
-  }
 
-  .todo {
-    display: grid;
-    grid-template-columns: 2rem 1fr 2rem;
-    grid-gap: 0.5rem;
-    align-items: center;
-    margin: 0 0 0.5rem 0;
-    padding: 0.5rem;
-    background-color: white;
-    border-radius: 8px;
-    filter: drop-shadow(2px 4px 6px rgba(0, 0, 0, 0.1));
-    transform: translate(-1px, -1px);
-    transition: filter 0.05s, transform 0.05s;
-  }
 
   .done {
     transform: none;
     opacity: 0.4;
     filter: drop-shadow(0 0 1px rgba(0, 0, 0, 0.1));
+  }
+
+
+  .interaction input {
+    flex: 1;
+    padding: 0.5em 2em 0.5em 0.8em;
+    border-radius: 3px;
   }
 
   form.question {
@@ -196,11 +184,6 @@
     flex: 1;
   }
 
-  .interaction input {
-    flex: 1;
-    padding: 0.5em 2em 0.5em 0.8em;
-    border-radius: 3px;
-  }
 
   button.toggle {
     border: 1px solid rgba(0, 0, 0, 0.2);
@@ -230,4 +213,5 @@
     opacity: 0;
     background-image: url("data:image/svg+xml,%3Csvg width='24' height='24' viewBox='0 0 24 24' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M20.5 2H3.5C2.67158 2 2 2.67157 2 3.5V20.5C2 21.3284 2.67158 22 3.5 22H20.5C21.3284 22 22 21.3284 22 20.5V3.5C22 2.67157 21.3284 2 20.5 2Z' fill='%23676778' stroke='%23676778' stroke-width='1.5' stroke-linejoin='round'/%3E%3Cpath d='M17 2V11H7.5V2H17Z' fill='white' stroke='white' stroke-width='1.5' stroke-linejoin='round'/%3E%3Cpath d='M13.5 5.5V7.5' stroke='%23676778' stroke-width='1.5' stroke-linecap='round'/%3E%3Cpath d='M5.99844 2H18.4992' stroke='%23676778' stroke-width='1.5' stroke-linecap='round'/%3E%3C/svg%3E%0A");
   }
+
 </style>
