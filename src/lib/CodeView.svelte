547aaeb59d19d778cf9875c24425c4183b511f88
Matthew Mirman <matt@extensional.ai>
2022-10-08 16:26:42 +0200

    code mirror

--- src/lib/CodeView.svelte
+++ src/lib/CodeView.svelte
@@ -1,19 +1,37 @@
 <script  lang="ts">
-  import { EditorView, Decoration, WidgetType, type DecorationSet, keymap } from '@codemirror/view';
-  import { basicSetup } from 'codemirror';
-  import { EditorState, Compartment, StateField, StateEffect, SelectionRange, EditorSelection, type ChangeSpec } from '@codemirror/state';
+  import { EditorView, Decoration, WidgetType, type DecorationSet } from '@codemirror/view';
+  import { EditorState, Compartment, StateField, StateEffect, EditorSelection } from '@codemirror/state';
 
   import { onMount } from 'svelte';
-
-
   import { javascript } from '@codemirror/lang-javascript';
+
+  import { lineNumbers, highlightActiveLineGutter, highlightSpecialChars, drawSelection, dropCursor, keymap } from '@codemirror/view';  
+  import { foldGutter, syntaxHighlighting, defaultHighlightStyle, bracketMatching, foldKeymap } from '@codemirror/language';
+  import { defaultKeymap } from '@codemirror/commands';
+
+  
 
   export let selectionStart = 0;
   export let selectionEnd = 0;
   export let gencode : string = "";
 
-  let editor: Element;
+  const basicSetup = /*@__PURE__*/(() => [
+    lineNumbers(),
+    highlightActiveLineGutter(),
+    highlightSpecialChars(),
+    foldGutter(),
+    drawSelection(),
+    EditorState.allowMultipleSelections.of(false),
+    syntaxHighlighting(defaultHighlightStyle, { fallback: true }),
+    bracketMatching(),
+    keymap.of([
+        ...defaultKeymap,
+        ...foldKeymap
+    ])
+])();
 
+
+  let editor: Element;
   let myView: EditorView;
   class CursorWidget extends WidgetType {
     constructor() {
@@ -94,6 +112,7 @@
       extensions: [basicSetup, language.of(javascript()), EditorState.readOnly.of(true)],
       parent: editor
     });
+
     const doclen = myView.state.doc.length;
     myView.dispatch({ selection : EditorSelection.cursor(doclen)});
     updateSelect();
@@ -122,4 +141,5 @@
     on:click={updateSelect}
     on:select={updateSelect}
     bind:this={editor}
+    class="codeView"
   />
