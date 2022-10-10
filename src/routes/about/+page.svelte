adcd204c12d5c8de6970a81dfb360af62d1985e9
Matthew Mirman <matt@extensional.ai>
2022-10-10 17:14:02 +0200

    beta

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -24,6 +24,10 @@
 	<p> 2. Tell codact what you want to do, create, or know. </p>
 	<p> 3. Click the fork icon to revert to a previous point in the code. </p>
 
+	<h1> Disclaimer </h1>
+
+	This product is still in beta.  Use it at your own risk.   Don't share malicious code. Don't give it your personal information. 
+
 </div>
 
 <style>
