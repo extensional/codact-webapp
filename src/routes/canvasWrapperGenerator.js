cd89b30ecec0533d63f51bb4ea07d9e5bcc79811
Matthew Mirman <matt@extensional.ai>
2022-10-10 13:54:33 +0200

    better basic styling

--- src/routes/canvasWrapperGenerator.js
+++ src/routes/canvasWrapperGenerator.js
@@ -3,5 +3,5 @@
  */
 export function canvasWrapperGenerator(js) {
   js = ` window.frameElement.addEventListener("load",() => { ${js} });`;
-  return `<html><head><script>${js}</script><style>html{height:100%; width:100%;} body{height:100%; width:100%; margin: 0;} canvas{height:100%; width:100%; background-color: #282c34;}</style></head><body><canvas id="myCanvas"></canvas></body></html>`;
+  return `<html><head><script>${js}</script><style>html{height:100%; width:100%;} body{height:100%; width:100%; margin: 0;} canvas{height:100%; width:100%; background-color: white;}</style></head><body><canvas id="myCanvas"></canvas></body></html>`;
 }
