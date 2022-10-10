05f2f3d6e3598433fb100610998ca1f569011f5d
James Spoor <james.spoor7@gmail.com>
2022-10-10 10:42:50 +0200

    cursor added back

--- src/lib/CodeView.svelte
+++ src/lib/CodeView.svelte
@@ -1,39 +1,18 @@
 <script  lang="ts">
-  import { EditorView, Decoration, WidgetType, type DecorationSet } from '@codemirror/view';
-  import { EditorState, Compartment, StateField, StateEffect, EditorSelection } from '@codemirror/state';
+  import { EditorView,  } from '@codemirror/view';
+  import { EditorState, Compartment, EditorSelection } from '@codemirror/state';
 
   import { onMount } from 'svelte';
   import { javascript } from '@codemirror/lang-javascript';
 
   import { lineNumbers, highlightActiveLineGutter, highlightSpecialChars, drawSelection, dropCursor, keymap } from '@codemirror/view';  
-  import { foldGutter, syntaxHighlighting, HighlightStyle, bracketMatching, foldKeymap } from '@codemirror/language';
+  import { foldGutter, bracketMatching, foldKeymap } from '@codemirror/language';
   import { defaultKeymap } from '@codemirror/commands';
   import { oneDark } from "@codemirror/theme-one-dark"  
 
   export let selectionStart = 0;
   export let selectionEnd = 0;
   export let gencode : string = "";
-
-  const editorTheme = EditorView.theme({
-    "&": {
-    color: "white",
-    backgroundColor: "#034"
-  },
-  ".cm-content": {
-    caretColor: "#0e9"
-  },
-  // "&.cm-focused .cm-cursor": {
-  //   borderLeftColor: "#0e9"
-  // },
-  "&.cm-focused .cm-selectionBackground, ::selection": {
-    backgroundColor: "#074"
-  },
-  ".cm-gutters": {
-    backgroundColor: "#045",
-    color: "#ddd",
-    border: "none"
-  }
-  }, {dark: true})
 
   const basicSetup = /*@__PURE__*/(() => [
     lineNumbers(),
