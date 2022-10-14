2caaed93cc698cb00bd616ed77c517d7faaa0b77
Matthew Mirman <matt@extensional.ai>
2022-10-14 16:04:27 -0700

    changes title on the server

--- src/routes/api/updateTitle.json/+server.js
+++ src/routes/api/updateTitle.json/+server.js
@@ -2,6 +2,9 @@
 import * as api from '$lib/codegen';
 import { json } from '@sveltejs/kit';
 
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
 
 export async function POST({ request }) {
   
@@ -12,20 +15,28 @@ export async function POST({ request }) {
     const completion = await api.openai.createCompletion({
         model: 'text-davinci-002',
         temperature: 1,
-        max_tokens: 10,
+        max_tokens: 7,
         n: 1,
         prompt
       });
     const compl = completion.data.choices === undefined ? '' : completion.data.choices[0].text;
     
     function removeQuotes(s){
+      s = s.trim();
+
       if (s[0] == '"')
         s = s.slice(1);
       if (s.at(-1) == '"')
         s = s.slice(0,-1);
       return s;
     }
-    
+    const newtitle = compl === undefined ? 'untitled' : removeQuotes(compl);
 
-    return json({ title: compl === undefined ? 'untitled' : removeQuotes(compl) });
+    prisma.interaction.update({
+      where: { gen: gen },
+      data : { title: newtitle}
+    });
+
+
+    return json({ title: newtitle });
 }
