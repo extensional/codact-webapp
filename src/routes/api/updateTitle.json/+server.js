e27fe0520e0f52e9a0ac43ea292755b726fb35df
Matthew Mirman <matt@extensional.ai>
2022-10-14 15:41:05 -0700

    added server endpoint for openai

--- src/routes/api/updateTitle.json/+server.js
+++ src/routes/api/updateTitle.json/+server.js
@@ -5,19 +5,27 @@ import { json } from '@sveltejs/kit';
 
 export async function POST({ request }) {
   
-    /*
-    const prompt = "here is a prompt";
+    const {gen, code} = await request.json();
+
+    const prompt = "```"+code+"```\nWhat title would you give to the file containing this code?\nTitle:";
 
     const completion = await api.openai.createCompletion({
         model: 'text-davinci-002',
         temperature: 1,
-        max_tokens: 5,
+        max_tokens: 10,
         n: 1,
         prompt
       });
     const compl = completion.data.choices === undefined ? '' : completion.data.choices[0].text;
-    */
+    
+    function removeQuotes(s){
+      if (s[0] == '"')
+        s = s.slice(1);
+      if (s.at(-1) == '"')
+        s = s.slice(0,-1);
+      return s;
+    }
+    
 
-
-    return json({ title: "untitled" }); // compl === undefined ? '' : compl;
+    return json({ title: compl === undefined ? 'untitled' : removeQuotes(compl) });
 }
